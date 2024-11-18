9a576f423bf69c7105e1034606ccbefe471a6be0
Omar Ahmed Mohammed Mclad <162033259+omarmclad@users.noreply.github.com>
2024-11-18 18:12:55 +0200

    Create G. Too Easy again and again.cpp

---
+++ G. Too Easy again and again.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define no {cout << "NO\n";}
+#define McLAD ios_base::sync_with_stdio(0); cin.tie(0);
+signed main() {
+    McLAD
+  // make sure to ask the question before you wait for the response
+    int x;
+    cin>>x;
+    //check if x is odd or even
+    if(x%2==0){
+        x--;
+    }
+    //print the number with 2 decreasing steps
+    for(int i=x;i>0;i-=2){
+        cout<<i<<" ";
+    }
+    return 0;
+}

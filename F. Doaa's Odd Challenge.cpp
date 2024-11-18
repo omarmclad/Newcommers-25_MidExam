e0524cf9845426d8b880fa78a3419780117083d3
Omar Ahmed Mohammed Mclad <162033259+omarmclad@users.noreply.github.com>
2024-11-18 18:03:44 +0200

    Update F. Doaa's Odd Challenge.cpp

--- F. Doaa's Odd Challenge.cpp
+++ F. Doaa's Odd Challenge.cpp
@@ -1,1 +1,30 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long 
+#define McLAD ios_base::sync_with_stdio(0); cin.tie(0);
+signed main() {
+    McLAD
+    //loop through all numbers from 1 to n
+    //for each number add their odd digits 
+    // example 721 -> 1+7=8
+    // if the sum of odd digits in number n is bigger than 15 ignore it 
+    int n,z;
+    cin>>n;
+    int sum2=0;
+    for(int i=1;i<n+1;i++){
+        int x=i,sum=0;
+        while(x>0)
+        {   
+            if((x%10)%2==1){
+                sum+=(x%10);
+            }
+            x/=10;
+        }
+        if(sum<=15){
+            sum2+=sum;
+        }       
+    }
+    cout<<sum2<<endl;
+    return 0;
+}

98412b9274c1f9ecde1f2e83f3b3862f3cb30d3b
Omar Ahmed Mohammed Mclad <162033259+omarmclad@users.noreply.github.com>
2024-11-18 17:23:45 +0200

    Create D. Unraveling the Power of Unknown.cpp

---
+++ D. Unraveling the Power of Unknown.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long 
+#define McLAD ios_base::sync_with_stdio(0); cin.tie(0);
+signed main() {
+    McLAD
+      //change of base rule
+      double x,y;
+      cin>>x>>y;
+      cout<<fx(9)<<log(y)/log(x);
+    return 0;
+}

c02ba046d2d19cdf86dd2d01074a6be51deef9b6
Omar Ahmed Mohammed Mclad <162033259+omarmclad@users.noreply.github.com>
2024-11-18 17:17:55 +0200

    Create C-Mohamed and Eid.cpp

---
+++ C-Mohamed and Eid.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long 
+#define McLAD ios_base::sync_with_stdio(0); cin.tie(0);
+signed main() {
+    McLAD
+  // make sure to ask the question before you wait for the response
+    string s;
+    while(true){
+        cout<<"Mohamed and Eid are brothers Mohamed died who is left?"<<endl;
+        cin>>s;
+        if(s!="Eid"){
+            no
+            break;
+        }
+    }
+    return 0;
+}

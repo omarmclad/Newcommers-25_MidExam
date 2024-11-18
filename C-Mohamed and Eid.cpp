2b5098b665b5872a3f9402aacd91ba982b529ab8
Omar Ahmed Mohammed Mclad <162033259+omarmclad@users.noreply.github.com>
2024-11-18 18:06:55 +0200

    Update C-Mohamed and Eid.cpp

--- C-Mohamed and Eid.cpp
+++ C-Mohamed and Eid.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-#define int long long 
+#define int long long
+#define no {cout << "NO\n";}
 #define McLAD ios_base::sync_with_stdio(0); cin.tie(0);
 signed main() {
     McLAD
